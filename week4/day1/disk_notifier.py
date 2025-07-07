e7bcaa1691f129f585412ac62eef0b4a451b80d2
Ramya <103323240+Ramyaa-ar@users.noreply.github.com>
2025-07-07 16:53:08 +0530

    Update disk_notifier.py

--- week4/day1/disk_notifier.py
+++ week4/day1/disk_notifier.py
@@ -2,20 +2,16 @@ import shutil
 import smtplib
 from email.message import EmailMessage
 
-# ----------------------------------------
-# Check disk usage
-# ----------------------------------------
+
 total, used, free = shutil.disk_usage("/mnt/c/")
 percent_used = used / total * 100
 
-# ----------------------------------------
-# Email setup
-# ------------------------com----------------
+
 sender_email = "21bcs463@gecskp.ac.in"      # <-- Your Gmail address
 receiver_email = "21bcs463@gecskp.ac.in"  # <-- Can be same as sender
 app_password = "ihpmkdukuydxgwkg"              # <-- Your App Password (NO SPACES!)
 
-# Create email content
+
 msg = EmailMessage()
 msg.set_content(f"""
 Hello,
@@ -35,7 +31,7 @@ msg['Subject'] = f"Daily Disk Usage Report — Used: {percent_used:.2f}%"
 msg['From'] = sender_email
 msg['To'] = receiver_email
 
-# Send email
+
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(sender_email, app_password)
     smtp.send_message(msg)
